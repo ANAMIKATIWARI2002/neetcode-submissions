b5ec7b4792c7a9287bf476d9fdadb3529d74596c
Anamika Tiwari <TIWARIANAMIKA2002@GMAIL.COM>
2026-06-03 22:54:03 +0530

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,51 @@
+//O(N + E α(N))
+//O(N): Space
+
+
+
+class Solution {
+    vector<int> parent, rankv;
+
+    int findParent(int node) {
+        if (parent[node] != node) {
+            parent[node] = findParent(parent[node]);
+        }
+        return parent[node];
+    }
+
+    bool unite(int a, int b) {
+        int pa = findParent(a);
+        int pb = findParent(b);
+
+        if (pa == pb) return true;    //they belong to same component already and an extra edge will create a cycle.
+        
+
+        if (rankv[pa] < rankv[pb]) {
+            parent[pa] = pb;
+        } 
+        else if (rankv[pa] > rankv[pb]) {
+            parent[pb] = pa;
+        } 
+        else {
+            parent[pb] = pa;
+            rankv[pa]++;
+        }
+
+        return false;
+    }
+
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        parent.resize(n + 1);
+        rankv.assign(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) parent[i] = i;
+
+        for (auto& e : edges) {
+            if (unite(e[0], e[1])) return e;
+        }
+
+        return {};
+    }
+};
