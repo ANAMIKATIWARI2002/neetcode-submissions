ac7ae2763e95275ce2f941b7ec439556256aaf05
Anamika Tiwari <TIWARIANAMIKA2002@GMAIL.COM>
2026-06-09 20:45:34 +0530

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.cpp
@@ -0,0 +1,77 @@
+// class Solution {
+// public:
+//      Time Complexity Worst Case O(E!)
+//     vector<string> findItinerary(vector<vector<string>>& tickets) {
+//         unordered_map<string, vector<string>> adj;
+//         for (auto& ticket : tickets) {
+//             adj[ticket[0]];
+//         }
+
+//         sort(tickets.begin(), tickets.end());
+//         for (auto& ticket : tickets) {
+//             adj[ticket[0]].push_back(ticket[1]);
+//         }
+
+//         vector<string> res = {"JFK"};
+//         dfs("JFK", res, adj, tickets.size() + 1);
+//         return res;
+//     }
+
+// private:
+//     bool dfs(const string& src, vector<string>& res,
+//              unordered_map<string, vector<string>>& adj, int targetLen) {
+//         if (res.size() == targetLen) {
+//             return true;
+//         }
+
+//         if (adj.find(src) == adj.end()) {
+//             return false;
+//         }
+
+//         vector<string> temp = adj[src];
+//         for (int i = 0; i < temp.size(); ++i) {
+//             string v = temp[i];
+//             adj[src].erase(adj[src].begin() + i);
+//             res.push_back(v);
+//             if (dfs(v, res, adj, targetLen)) return true;
+//             adj[src].insert(adj[src].begin() + i, v);
+//             res.pop_back();
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+// Hierholser algorithm - greedy approach.
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, deque<string>> adj;
+        for (auto& ticket : tickets) {
+            adj[ticket[0]].push_back(ticket[1]);
+        }
+        
+        for (auto& [src, dests] : adj) {
+            sort(dests.rbegin(), dests.rend());
+        }
+
+        vector<string> res;
+        dfs("JFK", adj, res);
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+private:
+    void dfs(const string& src, unordered_map<string,
+             deque<string>>& adj, vector<string>& res) {
+        while (!adj[src].empty()) {
+            string dst = adj[src].back();
+            adj[src].pop_back();
+            dfs(dst, adj, res);
+        }
+        res.push_back(src);
+    }
+};
